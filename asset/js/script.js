9cb7da342d762a2e26cace43df0287b5d1f77627
Libw-I <Libw-I@Libw.cc>
2024-11-01 10:52:38 +0800

    feat: 增强验证码相关功能

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // 显示所有 cf-turnstile 元素
-    document.querySelectorAll('.cf-turnstile').forEach(el => el.style.display = 'block');
+    // 初始化变量
     let loading = false;
+    const submitButton = document.getElementById('submit');
+
+    // 初始化按钮
+    submitButton.disabled = true;
+    submitButton.classList.add('loading');
+    submitButton.textContent = '请等待验证码验证完成...';
 
     // 显示提示信息的函数
     const showAlert = (type, message) => {
@@ -10,16 +15,53 @@ document.addEventListener('DOMContentLoaded', () => {
         alertEl.textContent = message;
     };
 
-    const submitButton = document.getElementById('submit');
+    // 渲染 Turnstile 小部件
+    let widgetId = turnstile.render('#turnstile-widget', {
+        sitekey: '0x4AAAAAAAylDH0pXEVAkn1K',
+        'retry': 'never',
+        'refresh-expired': 'manual',
+        'refresh-timeout': 'manual',
+        'feedback-enabled': 'false',
+        callback: () => {
+            submitButton.disabled = false;
+            submitButton.classList.remove('loading');
+            submitButton.textContent = '生成短链';
+        },
+        'error-callback': () => {
+            submitButton.classList.remove('loading');
+            submitButton.textContent = '请尝试刷新页面';
+            showAlert('error', '验证码在验证时出错, 请尝试刷新页面');
+        },
+        'expired-callback': () => {
+            submitButton.classList.remove('loading');
+            submitButton.textContent = '请重新通过验证码';
+            showAlert('error', '验证码的令牌已过期, 请重新验证');
+        },
+        'unsupported-callback': () => {
+            submitButton.classList.remove('loading');
+            submitButton.textContent = '请更新浏览器后再来';
+            showAlert('error', '抱歉, 您的浏览器过旧, 请更新后再来');
+        },
+        'timeout-callback': () => {
+            submitButton.classList.remove('loading');
+            submitButton.textContent = '请尝试刷新页面';
+            showAlert('error', '验证码在验证中超时, 请尝试刷新页面');
+        },
+    });
+
     submitButton.addEventListener('click', () => {
         if (loading) return; // 如果正在加载中，直接返回
+
+        if (!turnstile.getResponse()) {
+            showAlert('error', '请完成验证码');
+            return;
+        }
 
         // 获取用户输入的数据
         const url = document.getElementById('url').value;
         const slug = document.getElementById('slug').value;
         const password = document.getElementById('password').value;
         const email = document.getElementById('email').value;
-        const turnstileResponse = document.querySelector('[name="cf-turnstile-response"]').value;
 
         // 检查 URL 是否填写
         if (!url) {
@@ -39,7 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
         submitButton.classList.add('loading'); // 添加加载中的样式
 
         // 准备请求体数据
-        const body = {url, turnstileToken: turnstileResponse, slug, password, email};
+        const body = {url, turnstileToken: turnstile.getResponse(), slug, password, email};
 
         // 发送创建短链的请求
         fetch('/create', {
@@ -50,8 +92,9 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(response => response.json())
             .then(res => {
                 loading = false;
-                submitButton.disabled = false; // 重新启用提交按钮
-                submitButton.classList.remove('loading'); // 移除加载中的样式
+                submitButton.disabled = false;
+                submitButton.classList.remove('loading');
+                turnstile.reset(widgetId);
 
                 // 如果返回信息中包含错误，显示错误提示
                 if (res.message && res.code !== 200) {
@@ -62,21 +105,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 // 成功返回短链接，更新输入框的值并选中链接
                 document.getElementById('url').value = res.link;
                 document.getElementById('url').select();
-                showAlert('success', '完成，请复制下方的链接！');
+                showAlert('success', '完成, 请复制下方的链接！');
             })
             .catch(err => {
                 // 处理请求失败的情况
-                let errorMsg = '抱歉，短链创建失败，请重试';
+                let errorMsg = '抱歉, 短链创建失败, 请重试';
                 if (err.status === 429) {
-                    errorMsg = '操作速度过快，请稍后再试';
+                    errorMsg = '操作速度过快, 请稍后再试';
                 } else if (err.response && err.response.message) {
                     errorMsg = err.response.message;
                 }
                 console.error('Error details:', err);
                 showAlert('error', errorMsg);
                 loading = false;
-                submitButton.disabled = false; // 重新启用提交按钮
-                submitButton.classList.remove('loading'); // 移除加载中的样式
+                submitButton.disabled = false;
+                submitButton.classList.remove('loading');
+                turnstile.reset(widgetId);
             });
     });
 });
